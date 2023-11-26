84f22d3c6aa703720408c2d29b02d69abadf77ad
Patricio Zapata <117956808+St3m4n@users.noreply.github.com>
2023-11-26 22:55:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/skills/member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl',
+    bindToController: true
+  };
+}
